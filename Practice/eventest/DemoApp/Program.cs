7d480f5eb30c634f1e7181bfd18f74dd1c8353c7
Suyog Mayekar <114407875+msuyog8@users.noreply.github.com>
2022-11-03 13:42:17 +0530

    Add files via upload

---
+++ Practice/eventest/DemoApp/Program.cs
@@ -0,0 +1,61 @@
+﻿class Odd
+{
+
+  public bool isOdd(int n1)
+    {
+        if ((n1 % 2) != 0)
+        {
+            return true;  
+        }             
+
+        else 
+        {
+            return false;
+        }
+    }
+
+
+}
+
+
+
+
+
+class Program
+{
+    public static bool isEven(int n1)
+    {
+        if ((n1 % 2) == 0)
+        {
+            return true;  
+        }             
+
+        else 
+        {
+            return false;
+        }
+    }
+
+
+
+    public static void Main(string[] args)
+    {
+        int num = int.Parse(args[0]);
+        bool i = isEven(num);
+        if (i == true)
+        Console.WriteLine("The number is even");
+        else
+        Console.WriteLine("The number is odd");
+
+        Odd o = new Odd();
+
+        i = o.isOdd(num);
+        if (i == false)
+        Console.WriteLine("The number is EVEN");
+        else
+        Console.WriteLine("The number is ODD");
+    }
+
+
+
+}
