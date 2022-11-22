8c4440de1b01b528a114fbb09096aecaf782e8e8
Suyog Mayekar <114407875+msuyog8@users.noreply.github.com>
2022-11-22 15:13:15 +0530

    Add files via upload

---
+++ Problem1/ComputationLib/Computation.cs
@@ -0,0 +1,32 @@
+﻿namespace ComputationLib;
+
+
+public class Computation
+{
+    public delegate bool Check(int m);
+    public int Low{get; set;}
+    public int High{get; set;}
+
+public Computation(int low,int high)
+{
+Low = low;
+High = high;
+
+}
+public double GetSumOf(int Low, int High ,Check chk)
+{
+     for (int i = Low ; Low<High; Low++)
+     {
+            bool v = chk.Invoke(Low);
+            if(v == true)
+            {
+                double result =0;
+                return  result +Low;
+            }
+
+     }
+    
+}
+
+
+}
---
+++ Problem1/DemoApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using  ComputationLib;
+
+class Program
+{
+    
+
+    public static void Main(String[] args)
+    {
+        
+            Console.Write("Lower Number : ");
+            int l = int.Parse(Console.ReadLine());
+            Console.Write("Higher Number: ");
+            int h = int.Parse(Console.ReadLine());
+            var inv = new Computation(l, h);
+            
+            Console.WriteLine("Future value for riskless investment: {0:0.00}",inv.GetSumOf(l, h,isEven));
+            Console.WriteLine("Future value for riskless investment: {0:0.00}",inv.GetSumOf( l,  h, isOdd));
+            Console.WriteLine("Future value for riskless investment: {0:0.00}",inv.GetSumOf( l, h, isPrime));
+            bool isEven(int e)
+            {   
+                if (e%2==0)
+                return true;
+                else
+                return false;
+            }
+
+             bool isOdd(int o)
+            {   
+                if (o%2==0)
+                return false;
+                else
+                return true;
+            }
+            bool isPrime(int p)
+            {   
+               
+                if (p%2==0)
+                return false;
+                else
+                return true;
+            }
+
+
+
+
+
+
+
+    }
+}
